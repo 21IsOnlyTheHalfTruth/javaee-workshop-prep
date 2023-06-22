2061f74df54e792cb178413ce4e49788fc8ed994
Jacky Lee <jacky.lee@dedalus.eu>
2023-06-22 15:37:04 +0200

    added cache

--- src/main/java/com/dedalus/service/NinjaApiController.java
+++ src/main/java/com/dedalus/service/NinjaApiController.java
@@ -2,6 +2,7 @@ package com.dedalus.service;
 
 import com.dedalus.config.NinjaApiConfig;
 import com.dedalus.dto.AnimalDTO;
+import io.quarkus.cache.CacheResult;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 
 import javax.enterprise.context.ApplicationScoped;
@@ -20,12 +21,19 @@ public class NinjaApiController {
     @Inject
     NinjaApiConfig ninjaApiConfig;
 
-    private List<NinjaAnimalDTO> getAnimalsByName(String name){
+    @CacheResult(cacheName = "animal-cache")
+    public List<NinjaAnimalDTO> getAnimalsByName(String name){
         if(ninjaApiConfig.getNinjaApiKey() == null){
             throw new WebApplicationException("Environmentvariable API Key not configured", Response.Status.INTERNAL_SERVER_ERROR);
         }
+        try {
+            Thread.sleep(5000L);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
         return ninjaApiRestClient.getAnimalList(name, ninjaApiConfig.getNinjaApiKey());
     }
+
 
     public AnimalDTO populateAdditionalInformation(AnimalDTO animalDTO){
         //We mutate it instead of returning a new copy
